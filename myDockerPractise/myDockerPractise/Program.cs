1d5087a102912dd93268b30d5581227ea8a5e73f
chetan2510 <chetanchadha2594@gmail.com>
2020-05-05 00:02:00 +0200

    Creates a simple dotnet core app and added a docker file

---
+++ myDockerPractise/myDockerPractise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace myDockerPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine("Hello World!");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
